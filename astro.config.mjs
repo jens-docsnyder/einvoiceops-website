3d07f41ebaef2725a2a23afd0d0b750486b8ea4e
Jens Anttila <jensanttila@Jenss-MacBook-Air.local>
2026-04-28 11:27:06 +0300

    Initial Astro setup

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';                                            
+                                                            
+  export default defineConfig({
+    site: 'https://einvoiceops.eu',
+    trailingSlash: 'always',
+  });    
